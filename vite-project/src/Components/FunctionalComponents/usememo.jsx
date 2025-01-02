136d49d379bba89d65ff823ce12a64820f22fed9
Akshara095 <aksharark95@gmail.com>
2025-01-02 15:14:11 +0530

    day6

--- vite-project/src/Components/FunctionalComponents/usememo.jsx
+++ vite-project/src/Components/FunctionalComponents/usememo.jsx
@@ -1,7 +1,28 @@
+
+import React from "react";
+import { useState, useMemo } from "react";
+var slowFunction = (number)=> {
+    for(let i = 0;i<10000000000;i++)
+    return number*2
+} 
 var UseMemo = ()=>{
+    var [num, setNum] = useState(0);
+    var [theme, setTheme] = useState(true);
+    var themecolor = {
+        backgroundColor:(theme)?"black":"white",
+        color:(theme)?"white":"black"
+    }
+    var doublingANumber = useMemo(()=>{
+        return slowFunction(num)
+    },[num])
+      
+    
     return (
         <section>
             <h1>This is UseMemo example</h1>
+            <input type="number" value ={num} onChange={(e)=>setNum(e.target.value)}/>
+            <button onClick={()=>setTheme(theme = !theme)}>Toggle Theme</button>
+            <h2 style={themecolor}>{doublingANumber}</h2>
         </section>
     )
 }
